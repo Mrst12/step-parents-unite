936292afa26d7ed1e0c11653f274126f4a0e8743
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-05-16 19:13:23 +0000

    Add the database model for posts and relevant imports

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+""" Database model for step-parents-unite blog app """
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    """ Post Model """
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
