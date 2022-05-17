c9ef13817383631a0fa0163a0e5dcbc827632d5e
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-05-17 09:14:05 +0000

    Add summernote to the project for the admin panel

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,6 +1,11 @@
 """ Admin panel page"""
 
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """ Use summernote for our content """
+    summernote_fields = ('content')
