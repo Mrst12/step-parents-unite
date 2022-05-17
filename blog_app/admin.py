1e268773fc5dcd1a9a6cf686a81b42c9b0f58e76
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-05-17 09:37:42 +0000

    Add a filter, display and search box to our admin panel

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -10,4 +10,7 @@ class PostAdmin(SummernoteModelAdmin):
     """ Use summernote for our content """
 
     prepopulated_fields = {'slug': ('title' ,)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
