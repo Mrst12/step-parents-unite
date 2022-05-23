bacffce74ab97e88c9a9f5fa8eabe4a877f0a6ee
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-05-23 18:39:27 +0000

    Add the view for the Post list for the blog page

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,9 +2,19 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from .models import Post
 
 
 def index(request):
     """ home page view """
 
     return render(request, 'index.html')
+
+
+class PostList(generic.ListView):
+    """ blog page view """
+
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog_page.html'
+    paginate_by = 6
