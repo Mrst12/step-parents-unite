18fa1c3cf6017599523b84493d4b5ecb844e8518
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-05-24 11:32:20 +0000

    Add view for viewing the whole blog post when clicked by user

--- blog_app/views.py
+++ blog_app/views.py
@@ -18,3 +18,28 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog_page.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    """ View for the complete post"""
+
+    def get(self, request, slug, *args, **kwargs):
+        """ Get post information """
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "blog_details.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
