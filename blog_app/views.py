5205d2375856818e5792410ab15a45e4f1ce6940
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-05-25 08:34:28 +0000

    add the view for liking and unlikng a blog post

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,8 @@
 """ Views file for the blogs """
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -76,3 +77,18 @@ class BlogDetail(View):
                     "comment_form": CommentForm()
                 },
             )
+
+class BlogLike(View):
+    """ user to like/unlike blogs"""
+
+    def post(self, request, slug):
+        """ function for like/unlike"""
+
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('blog_details', args=[slug]))
