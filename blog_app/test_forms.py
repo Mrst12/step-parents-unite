2cea82ea00719a88bfc140c16850b14d6e395277
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-06-03 20:14:05 +0000

    add test for making sure content is required in form

--- blog_app/test_forms.py
+++ blog_app/test_forms.py
@@ -6,8 +6,18 @@ from .forms import BlogForm, CommentForm
 class TestBlogForm(TestCase):
     """ Unit test for blogs form """
     def test_post_title_is_required(self):
-        """ function """
+        """ title function """
         form = BlogForm(({'title': ''}))
         self.assertFalse(form.is_valid())
         self.assertIn('title', form.errors.keys())
         self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    
+    def test_post_content_is_required(self):
+        """ content function"""
+        form = BlogForm(({'content': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors.keys())
+        self.assertEqual(form.errors['content'][0], 'This field is required.')
+
+
