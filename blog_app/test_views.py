acc4f98c7f308c60ee72a1ad9416d649be060783
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-06-03 19:19:04 +0000

    add test for getting blog page

--- blog_app/test_views.py
+++ blog_app/test_views.py
@@ -7,6 +7,16 @@ from .models import Post
 class TestIndexViews(TestCase):
     """ Unit test index page view """
     def test_get_index_page(self):
+        """ function """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+class TestPostListViews(TestCase):
+    """ Unit test blog page view """
+    def test_get_blog_page(self):
+        """ function """
+        response = self.client.get('/blog_page/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog_page.html')
+
