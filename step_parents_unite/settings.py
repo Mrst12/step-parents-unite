628bdd3313a876687b73abe48781e58b798e0caa
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-06-03 20:41:12 +0000

    Take out the sqlite db no longer needed for testing

--- step_parents_unite/settings.py
+++ step_parents_unite/settings.py
@@ -105,17 +105,17 @@ WSGI_APPLICATION = 'step_parents_unite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
-#DATABASES = {
-#'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#
-#}
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
+}
 
 
 # Password validation
